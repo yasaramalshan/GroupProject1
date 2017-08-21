a90e3ae2b18a60760b662a45bf578fc01253d691
yasaramalshan <yasara.malshan@gmail.com>
2017-08-22 04:39:35 +0530

    recorrect , added code

---
+++ GroupProject1/Group Project/build/classes/Interface/TO/TOnext1.rs
@@ -0,0 +1,2 @@
+group.project1.TOnext1$1
+group.project1.TOnext1
---
+++ GroupProject1/Group Project/src/groupproject/GroupProject.java
@@ -0,0 +1,15 @@
+
+package groupproject;
+
+import Login.Login;
+
+/**
+ *
+ * @author Yasara JLP
+ */
+public class GroupProject {
+    public static void main(String[] args){
+        new Login().setVisible(true);
+    
+    }
+}
